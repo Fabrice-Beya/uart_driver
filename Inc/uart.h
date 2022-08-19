4c8340d5fa3f2a052fc04c378260924d38b22f8b
Fabrice Beya <byxfab001@myuct.ac.za>
2022-08-19 05:11:00 +0200

    uart init functionality

---
+++ Inc/uart.h
@@ -0,0 +1,51 @@
+/*
+ * uart.h
+ *
+ *  Created on: Aug 19, 2022
+ *      Author: fabricebeya
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include "stm32f3xx.h"
+
+#define ENABLE 						(|=)
+#define DISABLE						(&=~)
+
+#define GPIOA_CLOCK_EN				(1U << 17)
+#define UART2_CLOCK_EN				(1U << 17)
+
+#define GPIOA2_AF_0					(1U << 8)
+#define GPIOA2_AF_1					(GPIOA2_AF_0 << 1)
+#define GPIOA2_AF_2					(GPIOA2_AF_0 << 2)
+#define GPIOA2_AF_3					(GPIOA2_AF_0 << 3)
+#define GPIOA3_AF_0					(1U << 12)
+#define GPIOA3_AF_1					(GPIOA3_AF_0 << 1)
+#define GPIOA3_AF_2					(GPIOA3_AF_0 << 2)
+#define GPIOA3_AF_3					(GPIOA3_AF_0 << 3)
+
+#define GPIOA2_MODE_0				(1U << 4)
+#define GPIOA2_MODE_1				(GPIOA2_MODE_0 << 1)
+#define GPIOA3_MODE_0				(1U << 6)
+#define GPIOA3_MODE_1				(GPIOA3_MODE_0 << 1)
+
+#define BAUD_RATE_115200			115200
+#define BAUD_RATE_9600				9600
+#define UART2_CLK_SPEED				8000000	// Default clock speed
+
+#define UART2_TX_EN					(1U << 3)
+#define UART2_RX_EN					(1U << 2)
+
+#define UART2_STOP_BITS_0			(1U << 12)
+#define UART2_STOP_BITS_1			(1U << 13)
+
+#define UART2_CTS_CTRL				(1U << 9)
+#define UART2_RTS_CTRL				(1U << 10)
+
+#define UART2_EN					(1U)
+
+
+void uart_init(void);
+
+#endif /* UART_H_ */
---
+++ Src/uart.c
@@ -0,0 +1,140 @@
+/*
+ * uart.c
+ *
+ *  Created on: Aug 19, 2022
+ *      Author: fabricebeya
+ */
+
+#include "uart.h"
+
+void enable_gpio_clock(void);
+void enable_uart_clock(void);
+void configure_gpio(void);
+void confgiure_uart(void);
+void configure_baudrate_115200();
+void configure_baudrate_9600();
+uint16_t compute_baud_rate(uint32_t peripheral_clock, uint32_t baudrate);
+
+/*
+ * -	Enable clock access to Port A
+ * -	Enable clock access to UART 2 peripheral
+ * -	Configure pins PA2 and PA3 to be used for UART2 alternative function
+ * -	Configure UART
+ * -
+ */
+void uart_init(void)
+{
+	enable_gpio_clock();
+	enable_uart_clock();
+	configure_gpio();
+	confgiure_uart();
+}
+
+/*
+ *	Enable clock access to Port A
+ * 	Port A is connected to AHB2 bus
+ * 	Set bit 17 of RCC_AHBENR to 1
+ */
+void enable_gpio_clock(void)
+{
+	RCC->AHBENR |= GPIOA_CLOCK_EN;
+}
+
+
+/*
+ *	Enable clock access to UART 2
+ * 	UART 2 is connected to APB1
+ * 	Set bit 17 of RCC_APB1ENR to 1
+ *
+ */
+void enable_uart_clock(void)
+{
+	RCC->APB1ENR |= UART2_CLOCK_EN;
+}
+
+/*
+ * 	Configure GPIOA mode to AF for PA2 and PA3
+ * 	Set GPIOA_MODER MODER2[1:0] and MODER3[1:0] to 10
+ *	Configure pins PA2 and PA3 to be used as alternative function
+ * 	Set GPIOA_AFRL AFR2[3:0] and AFR3[3:0] to 0111(AF7) to set PA2 and PA3 to be used as TX and RX respectively
+ *
+ */
+void configure_gpio(void)
+{
+	GPIOA->MODER &= ~GPIOA2_MODE_0;
+	GPIOA->MODER |= GPIOA2_MODE_1;
+	GPIOA->MODER &= ~GPIOA3_MODE_0;
+	GPIOA->MODER |= GPIOA3_MODE_1;
+
+
+	GPIOA->AFR[0] |= GPIOA2_AF_0;
+	GPIOA->AFR[0] |= GPIOA2_AF_1;
+	GPIOA->AFR[0] |= GPIOA2_AF_2;
+	GPIOA->AFR[0] &= ~GPIOA2_AF_3;
+
+	GPIOA->AFR[0] |= GPIOA3_AF_0;
+	GPIOA->AFR[0] |= GPIOA3_AF_1;
+	GPIOA->AFR[0] |= GPIOA3_AF_2;
+	GPIOA->AFR[0] &= ~GPIOA3_AF_3;
+}
+
+
+
+
+/*
+ *	Configure UART 2
+ *	Set baud rate i.e the transfer rate to 115200
+ *	Enable TX and RX mode in the control registers
+ *	Set the data size to be 8bits
+ *	Set number of stop bits to 1: STOP[1:0]
+ *	Disable hardware flow control
+ *
+ */
+void confgiure_uart(void)
+{
+	configure_baudrate_115200();
+
+	USART2->CR1 |= UART2_TX_EN;
+	USART2->CR1 |= UART2_RX_EN;
+
+	USART2->CR2 &= ~UART2_STOP_BITS_0;
+	USART2->CR2 &= ~UART2_STOP_BITS_0;
+
+	USART2->CR3 &= ~UART2_CTS_CTRL;
+	USART2->CR3 &= ~UART2_RTS_CTRL;
+
+	USART2->CR1 |= UART2_EN;
+
+}
+
+/*
+ *	Configure baud rate register for 115200
+ *
+ */
+void configure_baudrate_115200()
+{
+	USART2->BRR = compute_baud_rate(UART2_CLK_SPEED, BAUD_RATE_115200);
+}
+
+/*
+ *	Configure baud rate register for 9600
+ *
+ */
+void configure_baudrate_9600()
+{
+	USART2->BRR = compute_baud_rate(UART2_CLK_SPEED, BAUD_RATE_9600);
+}
+
+/*
+ *	Configure baud rate value to pass into the baud rate register
+ *	Given the clock speed of the peripheral and the desired baud
+ *	rate we compute the value to pass into the register.
+ *
+ */
+uint16_t compute_baud_rate(uint32_t peripheral_clock, uint32_t baudrate)
+{
+	return ((peripheral_clock + (baudrate/2U))/baudrate);
+}
+
+
+
